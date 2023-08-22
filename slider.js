df5b9767f66db70a13d836b62b380574bb3af2b7
profkev <kmaina102@gmail.com>
2023-08-22 21:10:23 +0300

    my repository

---
+++ slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.slider').slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: false,
+      dots: false,
+      fade: true,
+      speed: 500,
+      infinite: true,
+      pauseOnHover: false,
+      cssEase: 'linear' // Set the CSS easing to linear
+    });
+  });
+  
